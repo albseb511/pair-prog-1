314bc3ba688bc0e7e1f4f834d051f448fa39a42f
albseb511 <albertsebastian511@gmail.com>
2020-08-23 14:52:41 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <code style={{whiteSpace: 'pre-wrap'}}>
+      {`
+      - Create a context by the name AuthContext
+      - create a AuthContextProvider component which will wrap the entire application around it
+      - create a isAuth and a toggleAuth function
+      - pass these values into your context provider
+      - create a navbar with a button to login or logout (change with the isAuth variable)
+      - create another component which will display the login status of the user
+      - these two components should not share props and the app state should be accessed through the context api`}
+      </code>
     </div>
   );
 }
